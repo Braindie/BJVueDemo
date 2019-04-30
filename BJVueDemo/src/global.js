14b70a673d34a7de39ab3c194ddb524f5e8957f0
zhangwenjun <zhangwenjun@zhangwenjundeMacBook-Pro.local>
2019-04-30 10:59:13 +0800

    bug

--- BJVueDemo/src/global.js
+++ BJVueDemo/src/global.js
@@ -5,9 +5,11 @@ import {
     XHeader,
     Group,
     XInput,
+    XButton
 } from 'vux';
 
 Vue.component('view-box',ViewBox);
 Vue.component('x-header',XHeader);
 Vue.component('group',Group);
 Vue.component('x-input', XInput);
+Vue.component('x-button', XButton);
