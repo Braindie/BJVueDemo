3a08651b49e86a4e06d4f18a5478c94ae2744e7a
张文军 <zhangwenjun02@tojoy.com>
2022-03-18 17:26:33 +0800

    fix

--- BJVueDemo/src/main.js
+++ BJVueDemo/src/main.js
@@ -1,38 +1,35 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import store from './store';
-
-
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import store from "./store";
 
 //引入矢量图标
-import '@/assets/fonts/iconfont.css';
+import "@/assets/fonts/iconfont.css";
 
 //引入组件
-import './global';
+import "./global";
 
 //引入全局样式
-import './styles/index.scss';
+import "./styles/index.scss";
 
 //引入淘宝自适应插件
-import  'lib-flexible';
+import "lib-flexible";
 
-import axios from 'axios'
+import axios from "axios";
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import * as uiv from 'uiv';
-
+import * as uiv from "uiv";
 
 //引入ElementUI
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
 //自定义全局组件（import 导入后的变量名 from 加载路径）
-import Tabbar from './components/tabbar';//这个应该是路由路径
-import BaseLayout from './components/base-layout';//这个应该是路由路径
-import Header from './components/header';//这个应该是路由路径
+import Tabbar from "./components/tabbar"; //这个应该是路由路径
+import BaseLayout from "./components/base-layout"; //这个应该是路由路径
+import Header from "./components/header"; //这个应该是路由路径
 
 Vue.use(Tabbar);
 Vue.use(BaseLayout);
@@ -41,15 +38,17 @@ Vue.use(Header);
 Vue.use(ElementUI);
 Vue.use(uiv);
 
-
 //root不在作为根组件，作为容器，加载三方组件（App.vue才是跟组件）
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+/*
+  程序入口，加载各种公共组件，默认页面通过router展示
+*/
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
--- BJVueDemo/src/router/index.js
+++ BJVueDemo/src/router/index.js
@@ -1,87 +1,96 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-const MainPage = () => import('../pages/main/MainPage.vue').then(m => m.default || m);
-const MainListPage = () => import('../pages/main/mainList/MainListPage.vue').then(m => m.default || m);
-const FlexboxPage = () => import('../pages/main/Flexbox/FlexboxPage.vue').then(m => m.default || m);
+const LayoutPage = () =>
+  import("../pages/layout/LayoutPage.vue").then(m => m.default || m);
+const MainListPage = () =>
+  import("../pages/layout/mainList/MainListPage.vue").then(m => m.default || m);
+const FlexboxPage = () =>
+  import("../pages/layout/flexBox/FlexboxPage.vue").then(m => m.default || m);
 
-const MinePage = () => import('../pages/mine/MinePage.vue').then(m => m.default || m);
+const FunPage = () =>
+  import("../pages/func/MinePage.vue").then(m => m.default || m);
 
-const AnimationPage = () => import('../pages/animation/AnimationPage.vue').then(m => m.default || m);
+const DrawPage = () =>
+  import("../pages/draw/AnimationPage.vue").then(m => m.default || m);
 
-const NetworkPage = () => import('../pages/network/NetworkPage.vue').then(m => m.default || m);
+const DataPage = () =>
+  import("../pages/data/NetworkPage.vue").then(m => m.default || m);
 
-const FramePage = () => import('../pages/frame/FramePage.vue').then(m => m.default || m);
+const FramePage = () =>
+  import("../pages/frame/FramePage.vue").then(m => m.default || m);
 
-const Login = () => import('../pages/login/Login.vue').then(m => m.default || m);
+const Login = () =>
+  import("../pages/login/Login.vue").then(m => m.default || m);
 
-Vue.use(Router)
+// 支持路由
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path:'',
-      name:'main',
-      component:MainPage,
-      meta:{
-        title:'Layout'
+      path: "",
+      name: "Layout",
+      component: LayoutPage,
+      meta: {
+        title: "Layout"
       }
     },
     {
-      path:'/mineListPage',
-      name:'mainList',
-      component:MainListPage,
-      meta:{
-        title:'Layout1'
+      path: "/mineListPage",
+      name: "mainList",
+      component: MainListPage,
+      meta: {
+        title: "Layout1"
       }
     },
     {
-      path:'/flexboxPage',
-      name:'flexbox',
-      component:FlexboxPage,
-      meta:{
-        title:'Layout2'
+      path: "/flexboxPage",
+      name: "flexbox",
+      component: FlexboxPage,
+      meta: {
+        title: "Layout2"
       }
     },
     {
-      path:'/animationPage',
-      name:'animation',
-      component:AnimationPage,
-      meta:{
-        title:'Draw'
+      path: "/animationPage",
+      name: "animation",
+      component: DrawPage,
+      meta: {
+        title: "Draw"
       }
     },
     {
-      path:'/networkPage',
-      name:'network',
-      component:NetworkPage,
-      meta:{
-        title:'Data'
+      path: "/networkPage",
+      name: "network",
+      component: DataPage,
+      meta: {
+        title: "Data"
       }
     },
     {
-      path:'/framePage',
-      name:'frame',
-      component:FramePage,
-      meta:{
-        title:'Frame'
+      path: "/framePage",
+      name: "frame",
+      component: FramePage,
+      meta: {
+        title: "Frame"
       }
     },
     {
-      path:'/minePage',
-      name:'mine',
-      component:MinePage,
-      meta:{
-        title:'Func'
+      path: "/minePage",
+      name: "mine",
+      component: FunPage,
+      meta: {
+        title: "Func"
       }
     },
     {
-      path:'/login',
-      name:'login',
-      component:Login,
-      meta:{
-        title:'登录'
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        title: "登录"
       }
     }
   ]
-})
+});
